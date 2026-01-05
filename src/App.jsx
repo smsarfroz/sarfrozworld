c3850bbc2e530aaeb0a3a3abe8781eca75805553
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2026-01-05 13:42:23 +0000

    bug: only home route works

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Outlet } from 'react-router';
 import './App.css'
 
 function App() {
@@ -6,6 +7,10 @@ function App() {
   return (
     <>
       <h1>Hello!</h1>
+
+      {/* <div className="commonBackground"> */}
+        <Outlet />
+      {/* </div> */}
     </>
   )
 }
