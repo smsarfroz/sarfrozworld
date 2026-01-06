c291455dfeedc18485523ac70cbfc6508a5cdb70
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2026-01-06 05:22:30 +0000

    bug: color doesn't apply to the icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router';
+import { sarfrozContext } from './sarfrozContext';
+import { FiHome } from "react-icons/fi";
+import { LuSearch } from "react-icons/lu";
+import { IoColorFill, IoPersonSharp } from "react-icons/io5";
+import { LuPenLine } from "react-icons/lu";
+import { MdLogout } from "react-icons/md";
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>Hello!</h1>
+      <div className="navigationRoutes">
+        <p className='siteName'>sarfrozworld</p>
 
-      {/* <div className="commonBackground"> */}
-        <Outlet />
-      {/* </div> */}
+        <div className="iconList">
+          <p><FiHome size={22} style={{ color: "DodgerBlue" }}/>Home</p>
+          <p><LuSearch size={22}/>Search</p>
+          <p><LuPenLine size={22}/>Post</p>
+          <p><IoPersonSharp size={22}/>Profile</p>
+          <p><MdLogout size={22}/>Logout</p>
+        </div>
+      </div>
+
+
+      <div className="commonBackground">
+        <sarfrozContext.Provider>
+          <Outlet />
+        </sarfrozContext.Provider>
+      </div>
     </>
   )
 }
