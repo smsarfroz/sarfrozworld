9f8e0dc8aedf31abc1de8a6dd0d354185fd310e0
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2026-01-14 07:14:12 +0000

    create conditional buttons for edit/save and conditional form containers and values

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,18 @@ const VITE_BASE_URL =  import.meta.env.VITE_BASE_URL || '/api';
 
 const useFetchData = () => {
   const api1 = `${VITE_BASE_URL}/users/profile`;
-  console.log('api1', api1);
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      console.log('outside try');
       try {
-        // const [res1] = await Promise.all([
-        //   fetch(api1)
-        // ]);
-        console.log('fetching api..');
-        const res1 = await fetch(api1, {
-          credentials: 'include'
-        });
-        console.log('res1 ', res1);
+        const [res1] = await Promise.all([
+          fetch(api1, {
+            credentials: 'include'
+          })
+        ]);
         if (!res1.ok) {
           throw new Error(`HTTP error! Status: ${Response.status}`);
         }
@@ -64,8 +59,7 @@ const useFetchData = () => {
 
 function App() {
   const { loading, error, userData } = useFetchData();
-  console.log('loading', loading);
-  console.log('userData ', userData);
+  // console.log('userData ', userData);
 
   if (loading) {
     return <Loading />;
@@ -80,7 +74,7 @@ function App() {
         <p className='siteName'>sarfrozworld</p>
         <div className="iconList">
           
-          <p><FiHome size={22} style={{ color: "DodgerBlue" }}/><Link to='/home'>home</Link></p>
+          <p><FiHome size={22}/><Link to='/home'>home</Link></p>
           <p><LuSearch size={22}/><Link to='/search'>Search</Link></p>
           <p><LuPenLine size={22}/><Link to='/post'>Post</Link></p>
           <p><IoPersonSharp size={22}/><Link to='/profile'>Profile</Link></p>
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,18 +1,82 @@
 import { Link } from 'react-router-dom';
 import styles from './Profile.module.css';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { sarfrozContext } from '../../sarfrozContext';
+import { MdOutlineModeEdit } from "react-icons/md";
+import { CiGlobe } from "react-icons/ci";
+import { RiGithubLine } from "react-icons/ri";
+import { LuBiohazard } from 'react-icons/lu';
+
 
 const VITE_BASE_URL =  import.meta.env.VITE_BASE_URL || '/api';
 
 
 const Profile = () => {
     const { userData } = useContext(sarfrozContext);
+    const { bio, followers, following, github, googleId, id, photo, username, website} = userData[0];
+    const [Bio, setBio] = useState("e");
+    const [Website, setWebsite] = useState("e");
+    const [Github, setGithub] = useState("e");
+    const [isEditing, setIsEditing] = useState(false);
+    // console.log('userData ', userData);
 
-    console.log('userData ', userData);
+    function handleEditClick() {
+        console.log('edit button clicked', isEditing);
+        setIsEditing(!isEditing);
+    }
     return (
         <div className={styles.profilePage}>
-            <h1>profile page</h1>
+
+            <img src={photo} alt="" className={styles.profilePhoto}/>
+            <p>{username}</p>
+            <div className="counterContainer">
+                <p>{followers == null ? 0 : followers}</p>
+                <p>Followers</p>
+            </div>
+            <div className="counterContainer">
+                <p>{following == null ? 0 : following}</p>
+                <p>Following</p>
+            </div>
+            <div className="counterContainer">
+                <p>{following == null ? 0 : followers}</p>
+                <p>Posts</p>
+            </div>
+
+            <p>{username}'s Posts</p>
+            
+            { isEditing ? 
+                <>
+                    <form action="">{Bio}</form>
+                    <form action="">{website}</form>
+                    <form action="">{github}</form>
+                </>
+                :
+                <>
+                    {Bio != "" ? 
+                        <p>{Bio}</p> 
+                        :
+                        null
+                    }
+                    {Bio != "" ? 
+                        <p><CiGlobe />{Website}</p> 
+                        :
+                        null
+                    }
+                    {Bio != "" ? 
+                        <p><RiGithubLine />{Github}</p> 
+                        :
+                        null
+                    }
+                </>
+            }
+            
+            { isEditing ? 
+                <button onClick={handleEditClick} className={styles.saveButton}>Save</button>
+                :
+                <div className={styles.editContainer}>
+                    <MdOutlineModeEdit fill="blue" size={30} onClick={handleEditClick} className={styles.editButton}/> <p>Edit</p>
+                </div>
+            }
         </div>
     )
 };
